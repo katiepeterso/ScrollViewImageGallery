b0ec32459acebd1ca04b9cc3d5b668bee5988ff1
katiepeterso <katiepeterso@outlook.com>
2015-09-07 12:48:32 -0700

    first commit

---
+++ ScrollViewImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGalleries
+//
+//  Created by Katherine Peterson on 2015-09-07.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewImageGalleries/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScrollViewImageGalleries
+//
+//  Created by Katherine Peterson on 2015-09-07.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
