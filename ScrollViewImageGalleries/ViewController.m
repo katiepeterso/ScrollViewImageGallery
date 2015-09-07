83b870425e9072650b9a8cbdaaceefc204c1e6f5
katiepeterso <katiepeterso@outlook.com>
2015-09-07 16:30:17 -0700

    adding image view constraints

--- ScrollViewImageGalleries/ViewController.m
+++ ScrollViewImageGalleries/ViewController.m
@@ -28,6 +28,24 @@
     
     self.image3.image = [UIImage imageNamed:@"Lighthouse"];
     
+//    NSLayoutConstraint *image1Width = [NSLayoutConstraint constraintWithItem:self.image1 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.imageGalleryView attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *image1Height = [NSLayoutConstraint constraintWithItem:self.image1 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.imageGalleryView attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *image1LeftMargin = [NSLayoutConstraint constraintWithItem:self.image1 attribute:NSLayoutAttributeLeftMargin relatedBy:NSLayoutRelationEqual toItem:self.imageGalleryView attribute:NSLayoutAttributeLeftMargin multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *image1TopMargin = [NSLayoutConstraint constraintWithItem:self.image1 attribute:NSLayoutAttributeTopMargin relatedBy:NSLayoutRelationEqual toItem:self.imageGalleryView attribute:NSLayoutAttributeTopMargin multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *imageGalleryWidth = [NSLayoutConstraint constraintWithItem:self.imageGalleryView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *imageGalleryHeight = [NSLayoutConstraint constraintWithItem:self.imageGalleryView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *imageGalleryLeftMargin = [NSLayoutConstraint constraintWithItem:self.imageGalleryView attribute:NSLayoutAttributeLeftMargin relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeftMargin multiplier:1.0 constant:0];
+//    
+//    NSLayoutConstraint *imageGalleryTopMargin = [NSLayoutConstraint constraintWithItem:self.imageGalleryView attribute:NSLayoutAttributeTopMargin relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTopMargin multiplier:1.0 constant:0];
+//
+//    [self.view addConstraints:@[imageGalleryHeight, imageGalleryLeftMargin, imageGalleryWidth, imageGalleryTopMargin]];
+//    [self.imageGalleryView addConstraints:@[image1Width, image1LeftMargin, image1Height, image1TopMargin]];
 }
 
 - (void)didReceiveMemoryWarning {
